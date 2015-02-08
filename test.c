705cab9eaf0035ece23e2e8932c3eb7f54be6958
rrahir <remi.rahir@student.uclouvain.be>
2015-02-08 15:52:39 +0100

    premier essai

---
+++ test.c
@@ -0,0 +1,10 @@
+//essai du code c sous gcc et windows
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+ 
+int main()
+{
+    printf("Test d'un bout de code en  C\n");
+    return 0;
+ }
