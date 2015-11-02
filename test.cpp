88b2ec0e1918e397818e2edf46d9d50b1d0fb52b
mdelcourt <martin.delcourt@uclouvain.be>
2015-11-02 16:49:11 +0100

    trololo

---
+++ test.cpp
@@ -0,0 +1,11 @@
+//essai du code c++ sous g++ et linux
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout<<"La première fois que je t'ai vu, je t'ai pris pour un bout de code en c++..."<<endl; 
+    int a  = 42;
+    cout<<42<<endl;
+    return 0;
+ }
